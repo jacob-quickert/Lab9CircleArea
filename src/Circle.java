5ca5d6984e85cc6267b90ad242844073a2c824f8
jacob <jacob.quickert@gmail.com>
2018-07-28 13:40:44 -0400

    added my comments for the assignment

--- src/Circle.java
+++ src/Circle.java
@@ -1,44 +1,43 @@
 import java.text.DecimalFormat;
 
-public class Circle {
-	private double userRadius;
+public class Circle { //"Circle" looks like it's spelled weird now. Thanks Java. :/
+					   // I need this for it to work and I don't remember why
+	private double userRadius; //I think it's my initial variable declaration
+							   //but it only works in this class
 	
-	public Circle (double radius) {
-		userRadius = radius;
+	public Circle (double radius) { //this is the circle object that takes a double for a parameter
+		userRadius = radius; 		//it fills the variable with double value from the parameter
 	}
 	
-	public double getCircumference() {
-		double circumference = 0.0;
-		circumference = userRadius * 2 * Math.PI;
+	public double getCircumference() { //calculate & return circumference
+		double circumference = userRadius * 2 * Math.PI;
 		
 		return circumference;
 	}
 	
-	public String getFormattedCircumference() {
+	public String getFormattedCircumference() {		
+		double circumference = getCircumference(); // call the circumference calculation
+		String formatCirc = formatNumber(circumference); // call the format method
 		
-		double circumference = getCircumference();
-		String formatCirc = formatNumber(circumference);
-		
-		return formatCirc;
+		return formatCirc; //--------------- this is the value I call from my main
 	}
 	
-	public double getArea() {
-		double area = 0.0;
-		area = userRadius * userRadius * Math.PI;
+	public double getArea() { //calculate & return area
+		double area = userRadius * userRadius * Math.PI;
 		
 		return area;
 	}
 	
 	public String getFormattedArea() {
-		double area = getArea();
-		String formatArea = formatNumber(area);
+		double area = getArea(); //------------ call the area calculation
+		String formatArea = formatNumber(area); // call the format method
 		
-		return formatArea;
+		return formatArea; //------ this is the value I call from my main
 	}
 	
-	private String formatNumber (double x) {
-		DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
-		String formattedNum = decimalFormat.format(x);
+	private String formatNumber (double x) { // format the double for 2 decimal places and a comma
+		DecimalFormat decimalFormat = new DecimalFormat("#,##0.00"); // define my pattern
+		String formattedNum = decimalFormat.format(x); //-- call the method for my double
 		
 		return formattedNum;
 	}
--- src/CircleApp.java
+++ src/CircleApp.java
@@ -3,26 +3,29 @@ import java.util.Scanner;
 public class CircleApp {
 
 	public static void main(String[] args) {
-
+		//create Scanner and declare variables for the counter and the while condition
 		Scanner scan = new Scanner(System.in);
-		double radius;
-		Circle userCircle;
 		int circleCount = 0;
 		String contInU = "y";
 
 		while (contInU.equalsIgnoreCase("y")) {
+			//start the counter at the top of the loop so it counts the first time through
 			circleCount++;
 			
-			radius = Validator.getDouble(scan, "Please enter a radius (1.00 - 100.00): ", 1, 100);
-			userCircle = new Circle(radius);
+			//use validator class to prompt, get, and validate user input
+			double radius = Validator.getDouble(scan, "Please enter a radius (1.00 - 100.00): ", 1, 100);
+			//create circle object instance
+			Circle userCircle = new Circle(radius);
 			
+			//print the results
 			System.out.println("Circumference: " + userCircle.getFormattedCircumference());
 			System.out.println("Area: " + userCircle.getFormattedArea());
-
+			
+			//prompt the user to continue
 			System.out.print("Continue? (y/n): ");
 			contInU = scan.nextLine();
 		}
-		
+		//print a goodbye and the number of circles
 		System.out.println("Thank you, you created " + circleCount + " circles. Goodbye.");
 		
 		scan.close();
